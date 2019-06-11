9e4984126eca497e2e0f665321e0cb23be796b0b
JSG1ILD <JONATHAN_S_GOE@HOMEDEPOT.COM>
2019-06-11 09:43:30 -0400

    added unit tests for actual validator

--- server/api/validators/roomNameValidator.test.js
+++ server/api/validators/roomNameValidator.test.js
@@ -1,35 +1,67 @@
 const {roomNameValidator, roomNameValidatorHelper} = require('./roomNameValidator')
 
-test('empty string is an invalid room name', async () => {
-    let result = await roomNameValidatorHelper('')
-    expect(result.error).toBe(true)
-})
-
-test('string with non-alphanumeric character is an invalid room name', async () => {
-    let result = await roomNameValidatorHelper('test!')
-    expect(result.error).toBe(true)
-})
-
-test('string with all spaces is considered an invalid room name', async () => {
-    let result = await roomNameValidatorHelper('   ')
-    expect(result.error).toBe(true)
-})
-
-test('null room name is invalid', async() => {
-    let result = await roomNameValidatorHelper(null)
-    expect(result.error).toBe(true)
-})
-
-test('undefined room name is invalid', async() => {
-    let result = await roomNameValidatorHelper(undefined)
-    expect(result.error).toBe(true)
-})
-
-test('valid room name is valid', async () => {
-    let result = await roomNameValidatorHelper('validRoomName')
-    expect(result.error).toBe(false)
-})
-
-test('smoke test for validator', async () => {
+describe('roomNameValidatorHelper', () => {
+    test('empty string is an invalid room name', async () => {
+        let result = await roomNameValidatorHelper('')
+        expect(result.error).toBe(true)
+    })
     
+    test('string with non-alphanumeric character is an invalid room name', async () => {
+        let result = await roomNameValidatorHelper('test!')
+        expect(result.error).toBe(true)
+    })
+    
+    test('string with all spaces is considered an invalid room name', async () => {
+        let result = await roomNameValidatorHelper('   ')
+        expect(result.error).toBe(true)
+    })
+    
+    test('null room name is invalid', async() => {
+        let result = await roomNameValidatorHelper(null)
+        expect(result.error).toBe(true)
+    })
+    
+    test('undefined room name is invalid', async() => {
+        let result = await roomNameValidatorHelper(undefined)
+        expect(result.error).toBe(true)
+    })
+    
+    test('valid room name is valid', async () => {
+        let result = await roomNameValidatorHelper('validRoomName')
+        expect(result.error).toBe(false)
+    })
+})
+
+describe('roomNameValidator', () => {
+    test('happy path for room name validator', async () => {
+        const req = {
+            params: {
+                roomName: "validRoomName"
+            }
+        }
+        const res = jest.fn()
+        const next = jest.fn()
+
+        await roomNameValidator(req, res, next)
+
+        expect(next.mock.calls.length).toBe(1)
+    })
+
+    test('unhappy path for room name validator', async () => {
+        const req = {
+            params: {
+                roomName: ""
+            }
+        }
+        const res = jest.fn()
+        res.status = jest.fn().mockReturnValue(res)
+        res.json = jest.fn().mockReturnValue(res)
+        const next = jest.fn()
+
+        await roomNameValidator(req, res, next)
+
+        expect(next.mock.calls.length).toBe(0)
+        expect(res.status.mock.calls.length).toBe(1)
+        expect(res.json.mock.calls.length).toBe(1)
+    })
 })
